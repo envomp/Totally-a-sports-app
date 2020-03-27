43f73ba7043b5b59250ef32c9cbf1319d89c1a4d
envomp <envomp@ttu.ee>
2020-03-27 21:03:03 +0200

    Location

---
+++ app/src/main/java/ee/taltech/spormapsapp/C.kt
@@ -0,0 +1,19 @@
+package ee.taltech.spormapsapp
+
+class C {
+    companion object {
+        val NOTIFICATION_CHANNEL = "default_channel"
+        val NOTIFICATION_ACTION_WP = "ee.taltech.spormapsapp.wp"
+        val NOTIFICATION_ACTION_CP = "ee.taltech.spormapsapp.cp"
+
+        val LOCATION_UPDATE_ACTION = "ee.taltech.spormapsapp.location_update"
+
+        val LOCATION_UPDATE_ACTION_LATITUDE = "ee.taltech.spormapsapp.location_update.latitude"
+        val LOCATION_UPDATE_ACTION_LONGITUDE = "ee.taltech.spormapsapp.location_update.longitude"
+
+        val NOTIFICATION_ID = 4321
+        val REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+
+    }
+}
--- app/src/main/java/ee/taltech/spormapsapp/MainActivity.kt
+++ app/src/main/java/ee/taltech/spormapsapp/MainActivity.kt
@@ -1,6 +1,22 @@
 package ee.taltech.spormapsapp
 
-
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
@@ -11,22 +27,21 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.hardware.SensorManager.SENSOR_DELAY_GAME
-import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.view.View
 import android.view.animation.Animation.RELATIVE_TO_SELF
 import android.view.animation.RotateAnimation
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.DrawableRes
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.*
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.map.*
 import java.lang.Math.toDegrees
 import kotlin.math.round
@@ -39,6 +54,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var stateUID = 0
     private lateinit var map: GoogleMap
+
+    private var locationServiceActive = false
+    private val broadcastReceiver = InnerBroadcastReceiver()
+    private val broadcastReceiverIntentFilter: IntentFilter = IntentFilter()
+
 
     // sensors
     lateinit var sensorManager: SensorManager
@@ -54,6 +74,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var lastMagnetometerSet = false
 
     // Header
+    private var userLatitude = 0.0;
+    private var userLongitude = 0.0;
     private var direction = 0
     private val direction_map: HashMap<Int, String> =
         hashMapOf(0 to "CENTERED", 1 to "NORTH-UP", 2 to "DIRECTION-UP", 3 to "CHOSEN-UP")
@@ -71,7 +93,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var overall_average_speed = 0.0
 
     //COL 2
-    private var CPs: MutableCollection<Marker> = mutableListOf() // capture points__ Permanent!
+    private var CP: Marker? = null // capture points__ Only one!
     private val CP_disance_overall = 0
     private val CP_distance_line = 0
     private var CP_average_speed = 0.0
@@ -86,6 +108,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // safe to call every time
+        createNotificationChannel()
+
+        if (!checkPermissions()) {
+            requestPermissions()
+        }
+
+        broadcastReceiverIntentFilter.addAction(C.LOCATION_UPDATE_ACTION)
+
         with(mapView) {
             // Initialise the MapView
             onCreate(savedInstanceState)
@@ -93,7 +124,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             getMapAsync {
                 map = it
                 MapsInitializer.initialize(applicationContext)
-//                map.isMyLocationEnabled = true
+                map.isMyLocationEnabled = true
                 map.uiSettings.isMyLocationButtonEnabled = true
                 mapActions(it)
 
@@ -120,21 +151,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 MarkerOptions().position(WP!!.position)
                     .title("Added a way point")
                     .draggable(true)
-                    .icon(vectorToBitmap(R.drawable.capturepoint, 100, 150))
+                    .icon(vectorToBitmap(R.drawable.waypoint, 100, 150))
             )
         }
 
-        var i = 0
-        for (marker: Marker in CPs) {
-            i += 1
-            marker.remove()
+        if (CP != null) {
+            CP!!.remove()
             map.addMarker(
-                MarkerOptions().position(marker.position)
-                    .title(String.format("Added a capture point nr: %s", i))
+                MarkerOptions().position(CP!!.position)
+                    .title("Added a way point")
                     .draggable(true)
                     .icon(vectorToBitmap(R.drawable.capturepoint, 100, 150))
             )
         }
+
 
     }
 
@@ -146,18 +176,16 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             setOnMapClickListener {
                 if (add_CP) {
 
-                    CPs.add(
-                        addMarker(
-                            MarkerOptions().position(LatLng(it.latitude, it.longitude))
-                                .title(String.format("Added a capture point nr: %s", CPs.size + 1))
-                                .draggable(true)
-                                .icon(vectorToBitmap(R.drawable.capturepoint, 100, 150))
-                        )
+                    CP = addMarker(
+                        MarkerOptions().position(LatLng(userLatitude, userLongitude))
+                            .title(String.format("Capture point"))
+                            .draggable(true)
+                            .icon(vectorToBitmap(R.drawable.capturepoint, 100, 150))
                     )
 
                     Toast.makeText(
                         this@MainActivity,
-                        String.format("Added a capture point nr: %s", CPs.size),
+                        String.format("Added a capture point"),
                         Toast.LENGTH_SHORT
                     )
                         .show()
@@ -182,6 +210,34 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
                 setColorsAndTexts()
             }
+
+            map.setOnMyLocationChangeListener { location ->
+
+                when (direction) {
+                    0 -> {
+                        // case CENTERED
+                        //                map.animateCamera(CameraUpdateFactory.newLatLngZoom(, 16f));
+                        val center: CameraUpdate = CameraUpdateFactory.newLatLng(
+                            LatLng(
+                                location.latitude,
+                                location.longitude
+                            )
+                        )
+                        val zoom: CameraUpdate = CameraUpdateFactory.zoomTo(20f)
+                        map.moveCamera(center)
+                        map.animateCamera(zoom)
+                    }
+                    1 -> {
+                        // case NORTH-UP
+                    }
+                    2 -> {
+                        // case DIRECTION-UP
+                    }
+                    3 -> {
+                        // case CHOSEN-UP
+                    }
+                }
+            }
         }
     }
 
@@ -189,6 +245,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         findViewById<Button>(R.id.start_or_stop).setBackgroundColor(resources.getColor(R.color.colorGreen))
         findViewById<Button>(R.id.start_or_stop).setOnClickListener {
             started = !started
+            if (locationServiceActive) {
+                // stopping the service
+                stopService(Intent(this, LocationService::class.java))
+
+            } else {
+                if (Build.VERSION.SDK_INT >= 26) {
+                    // starting the FOREGROUND service
+                    // service has to display non-dismissable notification within 5 secs
+                    startForegroundService(Intent(this, LocationService::class.java))
+                } else {
+                    startService(Intent(this, LocationService::class.java))
+                }
+
+            }
+            locationServiceActive = !locationServiceActive
             setColorsAndTexts()
             gameLoop()
         }
@@ -224,7 +295,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             // RESET EVERYTHING
             map.clear()
             WP = null
-            CPs = mutableListOf()
+            CP = null
             setColorsAndTexts()
 
             findViewById<Button>(R.id.reset).setBackgroundColor(resources.getColor(R.color.colorGreener))
@@ -256,13 +327,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         Handler().postDelayed(
             {
 
-                var lastUID = stateUID
+                val lastUID = stateUID
                 if (started && lastUID == stateUID) {
                     session_duration += 1
-
                     updateVisibleText()
+                    // broadcast new location to UI
+                    findViewById<Button>(R.id.start_or_stop).text = "$userLatitude   $userLongitude"
                     gameLoop()
-                    mapTransformations()
                 }
 
             },
@@ -271,7 +342,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     private fun mapTransformations() {
-
+        when (direction) {
+            0 -> {
+                // case CENTERED
+//                map.animateCamera(CameraUpdateFactory.newLatLngZoom(, 16f));
+            }
+            1 -> {
+                // case NORTH-UP
+            }
+            2 -> {
+                // case DIRECTION-UP
+            }
+            3 -> {
+                // case CHOSEN-UP
+            }
+        }
     }
 
     private fun setColorsAndTexts() {
@@ -351,7 +436,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onResume() {
         super.onResume()
         mapView.onResume()
-
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, broadcastReceiverIntentFilter)
         sensorManager.registerListener(this, accelerometer, SENSOR_DELAY_GAME)
         sensorManager.registerListener(this, magnetometer, SENSOR_DELAY_GAME)
         Log.d(TAG, "lifecycle onResume")
@@ -366,7 +451,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onStop() {
         super.onStop()
         mapView.onStop()
-
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver)
         sensorManager.unregisterListener(this, accelerometer)
         sensorManager.unregisterListener(this, magnetometer)
         Log.d(TAG, "lifecycle onStop")
@@ -460,4 +545,146 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             output[i] = output[i] + alpha * (input[i] - output[i])
         }
     }
+
+    // ============================================== NOTIFICATION CHANNEL CREATION =============================================
+    private fun createNotificationChannel() {
+        // when on 8 Oreo or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                C.NOTIFICATION_CHANNEL,
+                "Default channel",
+                NotificationManager.IMPORTANCE_LOW
+            );
+
+            //.setShowBadge(false).setSound(null, null);
+
+            channel.description = "Default channel"
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    // ============================================== PERMISSION HANDLING =============================================
+    // Returns the current state of the permissions needed.
+    private fun checkPermissions(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale =
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(
+                TAG,
+                "Displaying permission rationale to provide additional context."
+            )
+
+            Snackbar.make(
+                findViewById(R.id.activity_main),
+                "Hey, i really need to access GPS!",
+                Snackbar.LENGTH_INDEFINITE
+            )
+                .setAction("OK", View.OnClickListener {
+                    // Request permission
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        C.REQUEST_PERMISSIONS_REQUEST_CODE
+                    )
+                })
+                .show()
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                C.REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode === C.REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.count() <= 0 -> { // If user interaction was interrupted, the permission request is cancelled and you
+                    // receive empty arrays.
+                    Log.i(TAG, "User interaction was cancelled.")
+                    Toast.makeText(this, "User interaction was cancelled.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                grantResults[0] === PackageManager.PERMISSION_GRANTED -> {// Permission was granted.
+                    Log.i(TAG, "Permission was granted")
+                    Toast.makeText(this, "Permission was granted", Toast.LENGTH_SHORT).show()
+                }
+                else -> { // Permission denied.
+                    Snackbar.make(
+                        findViewById(R.id.activity_main),
+                        "You denied GPS! What can I do?",
+                        Snackbar.LENGTH_INDEFINITE
+                    )
+                        .setAction("Settings", View.OnClickListener {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri: Uri = Uri.fromParts(
+                                "package",
+                                BuildConfig.APPLICATION_ID, null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        })
+                        .show()
+                }
+            }
+        }
+
+    }
+
+    fun buttonWPOnClick(view: View) {
+        Log.d(TAG, "buttonWPOnClick")
+        sendBroadcast(Intent(C.NOTIFICATION_ACTION_WP))
+    }
+
+    fun buttonCPOnClick(view: View) {
+        Log.d(TAG, "buttonCPOnClick")
+        sendBroadcast(Intent(C.NOTIFICATION_ACTION_CP))
+    }
+
+
+    private inner class InnerBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d(TAG, intent!!.action)
+            when (intent.action) {
+                C.LOCATION_UPDATE_ACTION -> {
+                    userLatitude =
+                        intent.getDoubleExtra(C.LOCATION_UPDATE_ACTION_LATITUDE, 0.0)
+                    userLongitude =
+                        intent.getDoubleExtra(C.LOCATION_UPDATE_ACTION_LONGITUDE, 0.0)
+                }
+            }
+        }
+
+    }
+
+
 }
